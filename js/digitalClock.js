c4cf77ce3fc373a1f74baa6f8b8e068a61621c00
ZuryOphre <zury.martinez.saldivia@gmail.com>
2022-07-18 13:31:12 -0400

    fixed the time format

--- js/digitalClock.js
+++ js/digitalClock.js
@@ -1,9 +1,15 @@
 const showWatch = () =>{
     let date = new Date();
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let seg = date.getSeconds();
+    let hr = hourFormat(date.getHours());
+    let min = hourFormat(date.getMinutes());
+    let seg = hourFormat(date.getSeconds());
     document.getElementById('hour').innerHTML = `${hr}:${min}:${seg}`
+}
+
+const hourFormat = (hour)=>{
+    if(hour < 10)
+    hour = '0' + hour;
+    return hour;
 }
 
 setInterval(showWatch, 1000);
